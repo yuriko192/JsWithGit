c9b69183b690a3ea6e5124df3f8111efbf326f2d
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-12-18 15:42:23 +0700

    init

--- Script.js
+++ Script.js
@@ -1,1 +1,25 @@
-let x = 1;
+let fun = () => {
+    console.log("function");
+};
+
+function ff() {
+    console.log("function juga");
+}
+
+let dic = {
+    name: "asd",
+    age: 12,
+
+    printname: () => {
+        console.log(this.name);
+    }
+};
+
+let x = 5;
+
+for (i = 0; i < x; i++) {
+    console.log(i);
+}
+
+console.log(dic.name);
+dic.printname();
